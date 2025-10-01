c78e25d3b066283255ba458ea8540d0c9962a0f4
David Oh <general.davidclimbing@gmail.com>
2025-10-01 23:20:06 +0900

    remove junks

--- data-structure/stack/stack.js
+++ data-structure/stack/stack.js
@@ -31,21 +31,3 @@ class Stack {
 		this.items = [];
 	}
 }
-
-
-const stack = new Stack();
-stack.push(1);
-stack.push(2);
-stack.push(3);
-console.log(stack.peek());
-console.log(stack.pop());
-console.log(stack.peek());
-console.log(stack.isEmpty());
-console.log(stack.clear());
-console.log(stack.isEmpty());
-console.log(stack.peek());
-console.log(stack.pop());
-console.log(stack.peek());
-console.log(stack.isEmpty());
-console.log(stack.clear());
-console.log(stack.isEmpty());
